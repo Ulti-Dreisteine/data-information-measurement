f7be900727145f5abe76ae3f017a78070b4ab728
dreisteine-scu <dreisteine@stu.scu.edu.cn>
2021-05-13 00:08:30 +0800

    离散可视化还有点问题

--- core/data_partition/marginal_equiquantization.py
+++ core/data_partition/marginal_equiquantization.py
@@ -12,6 +12,7 @@ Created on 2021/05/12 16:45:21
 """
 
 from sklearn.preprocessing import MinMaxScaler
+import matplotlib.pyplot as plt
 from typing import Tuple
 import numpy as np
 import sys
@@ -42,27 +43,160 @@ def load_data() -> Tuple[np.ndarray, np.ndarray]:
     return x, y
 
 
+# ---- 离散化代码 -----------------------------------------------------------------------------------
+
+# 单个Cell.
+class Cell(object):
+    """离散化中的Cell, 即单元格
+    """
+
+    def __init__(self, arr: np.ndarray) -> None:
+        """初始化
+
+        :param arr: 存储有x,y坐标的二维数组, shape = (N, D=2)
+        :param bounds: x和y方向上的边界值, 如[[x_min, x_max], [y_min, y_max]]
+        """
+        self.arr = arr.copy()
+        self.N, self.D = arr.shape
+        self.bounds = self._get_arr_bounds()
+        self.area = self._cal_area()
+
+    def _get_arr_bounds(self):
+        return list(zip(np.min(self.arr, axis=0), np.max(self.arr, axis=0)))
+
+    def _cal_area(self):
+        return (self.bounds[0][1] - self.bounds[0][0]) * (self.bounds[1][1] - self.bounds[1][0])
+
+    def _get_marginal_partition_thres(self) -> list:
+        """各维度上按照边际概率(即样本数)相等的方式划分为两个子集
+        """
+        part_idx = self.N // 2  # 离散化位置idx
+        part_thres = []
+        for i in range(self.D):
+            arr_srt = self.arr[np.argsort(self.arr[:, i]), :]  # 对应维度值升序排列
+            marginal_part_value = arr_srt[part_idx, i]  # >=该值的样本被划分入新的cell
+            part_thres.append(marginal_part_value)
+        return part_thres
+
+    def get_marginal_partition_thres(self) -> list:
+        """获得各边际离散化阈值
+        """
+        self.part_thres = self._get_marginal_partition_thres()
+
+    def equiprob_partition(self) -> tuple:
+        """等概率离散化
+        """
+        # 先在x方向上分为左右两部分.
+        part_arr_l = self.arr[np.where(self.arr[:, 0] < self.part_thres[0])]
+        part_arr_r = self.arr[np.where(self.arr[:, 0] >= self.part_thres[0])]
+
+        # 再在y方向上继续切分.
+        part_arr_ul = part_arr_l[np.where(
+            part_arr_l[:, 1] >= self.part_thres[1])]
+        part_arr_ll = part_arr_l[np.where(
+            part_arr_l[:, 1] < self.part_thres[1])]
+
+        part_arr_ur = part_arr_r[np.where(
+            part_arr_r[:, 1] >= self.part_thres[1])]
+        part_arr_lr = part_arr_r[np.where(
+            part_arr_r[:, 1] < self.part_thres[1])]
+
+        return Cell(part_arr_ul), Cell(part_arr_ur), Cell(part_arr_ll), Cell(part_arr_lr)
+
+    def cal_P(self):
+        return self.N / self.area
+
+    def draw(self, linewidth: float = 0.5):
+        (xl, xu), (yl, yu) = self.bounds
+        proj_plt.plot([xl, xu], [yl, yl], 'k--', linewidth=linewidth)
+        proj_plt.plot([xu, xu], [yl, yu], 'k--', linewidth=linewidth)
+        proj_plt.plot([xu, xl], [yu, yu], 'k--', linewidth=linewidth)
+        proj_plt.plot([xl, xl], [yu, yl], 'k--', linewidth=linewidth)
+
+
+
+# 递归分裂.
+def single_step_partition(cell: Cell) -> Tuple[Cell, Cell, Cell, Cell]:
+    cell.get_marginal_partition_thres()
+    return cell.equiprob_partition()
+
+
+def recursively_partition(cell: Cell, eps: float=1E-2, N_min: int = 30) -> Tuple[Cell, Cell, Cell, Cell]:
+    """递归分裂离散化
+
+    :param arr: 待离散化样本数据
+    :param eps: 分裂前后概率密度相对变化, defaults to 1E-2
+    :param N_min: 叶子节点样本数下界, defaults to 10
+    """
+
+    cell_ul, cell_ur, cell_ll, cell_lr = single_step_partition(cell)
+
+    is_P_converged = True
+    is_N_limited = False
+
+    P = cell.cal_P()
+    for c in [cell_ul, cell_ur, cell_ll, cell_lr]:
+        if (np.abs(c.cal_P() - P) / P > eps):
+            is_P_converged = False
+        if c.N < N_min:
+            is_N_limited = True
+    
+    if (not is_P_converged) & (not is_N_limited):
+        return recursively_partition(cell_ul), recursively_partition(cell_ur), recursively_partition(cell_ll), recursively_partition(cell_lr)
+    else:
+        return cell_ul, cell_ur, cell_ll, cell_lr
+
+
+# 解析结果.
+def parse_partition_result(partition: tuple):
+    leafs = []
+    
+    def _parse(p: tuple):
+        for pi in p:
+            if type(pi) == Cell:
+                leafs.append(pi)
+            elif type(pi) == tuple:
+                _parse(pi)
+
+    _parse(partition)
+    return leafs
+
+
 if __name__ == '__main__':
     from src.settings import *
 
     # ---- 载入数据 ---------------------------------------------------------------------------------
 
     x, y = load_data()
-    
+
     # ---- 预处理 -----------------------------------------------------------------------------------
 
     # 数据归一化.
     scaler = MinMaxScaler()
     x = scaler.fit_transform(x.reshape(-1, 1))
     y = scaler.fit_transform(y.reshape(-1, 1))
-    # proj_plt.scatter(x, y, s=3)
+    proj_plt.scatter(x, y, s=3)
 
-    # ---- tmp -------------------------------------------------------------------------------------
+    # ---- 单个Cell测试 -----------------------------------------------------------------------------
 
-    # 有序列表按照元素个数二等分.
-    N = len(x)
+    arr = np.hstack((x, y))
+    cell = Cell(arr)
+
+    # cell.get_marginal_partition_thres()
+    # cell_ul, cell_ur, cell_ll, cell_lr = cell.equiprob_partition()
+
+    # cell_ul, cell_ur, cell_ll, cell_lr = single_step_partition()(cell)
+
+    partition_ul, partition_ur, partition_ll, partition_lr = recursively_partition(cell)
+
+    leaf_cells = []
+    for partition in [partition_ul, partition_ur, partition_ll, partition_lr]:
+        leaf_cells += parse_partition_result(partition)
     
-    
+    for c in leaf_cells:
+        c.get_marginal_partition_thres()
+        c.draw()
+
 
 
 
